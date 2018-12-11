8ba419b2b7bd65ef6b6ccc239fc11603d3fb12f3
jr <123456>
2018-12-11 14:58:18 +0800

    JR---PhotoApi

--- src/main/java/com/ydb/service/imp/PhotoServiceImp.java
+++ src/main/java/com/ydb/service/imp/PhotoServiceImp.java
@@ -3,11 +3,21 @@ package com.ydb.service.imp;
 import com.ydb.bean.ResultBean;
 import com.ydb.dao.IPhotoDao;
 import com.ydb.entity.Photo;
+import com.ydb.exception.ErrorFileFormatException;
 import com.ydb.service.IPhotoService;
+import net.coobird.thumbnailator.Thumbnailator;
+import net.coobird.thumbnailator.Thumbnails;
+import net.coobird.thumbnailator.util.ThumbnailatorUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
+
 /**
  * @program: com.ydb.dao
  * @description: PhotoServiceImp
@@ -20,17 +30,28 @@ public class PhotoServiceImp implements IPhotoService {
     @Autowired
     IPhotoDao photoDao;
 
+
     ResultBean<Photo> resultBean;
 
     @Override
-    public ResultBean<Photo> addPhoto(Photo photo) {
+    public ResultBean<Photo> addPhoto(MultipartHttpServletRequest request, Photo photo) throws IOException {
+        //判断文件格式
+        if (!judgeFormat()) {
+            throw new ErrorFileFormatException("上传的文件格式有误");
+        }
+        //保存图片到本地
+        saveOriginalPhoto(request, photo);//原始图片
+        saveThumPhoto(request, photo);//缩略图片
+        //保存图片信息到数据库
         photoDao.insertPhoto(photo);
+        //返回response
         resultBean = new ResultBean<>();
         resultBean.setStatus(ResultBean.SUCCSSED_CODE);
         resultBean.setMsg("添加成功");
         resultBean.setData(Arrays.asList(photo));
         return resultBean;
     }
+
 
     @Override
     public ResultBean<Photo> dropPhoto(Integer photoId) {
@@ -70,5 +91,38 @@ public class PhotoServiceImp implements IPhotoService {
         resultBean.setMsg("查询成功");
         resultBean.setData(Arrays.asList(photo));
         return resultBean;
+    }
+
+    private boolean judgeFormat() {
+        return false;
+    }
+
+    private void saveOriginalPhoto(MultipartHttpServletRequest request, Photo photo) throws IOException {
+        MultipartFile multipartFile = request.getFile("photo");
+        String contextPath = request.getSession().getServletContext().getRealPath("/") + "/originalphoto";
+        File file = new File(contextPath);
+        if (!file.exists()) {
+            file.mkdir();
+        }
+        String imageName = photo.getPhotoName() + "-" + System.currentTimeMillis() + "-" + multipartFile.getOriginalFilename();
+        File savePath = new File(contextPath, imageName);
+        multipartFile.transferTo(savePath);
+        photo.setPhotoOriginalUrl("http://localhost:8080/originalphoto/" + imageName);
+    }
+
+
+    private void saveThumPhoto(MultipartHttpServletRequest request, Photo photo) throws IOException {
+        MultipartFile multipartFile = request.getFile("photo");
+        String contextPath = request.getSession().getServletContext().getRealPath("/") + "/thumphoto";
+        File file = new File(contextPath);
+        if (!file.exists()) {
+            file.mkdir();
+        }
+        String imageName = photo.getPhotoName() + "-" + System.currentTimeMillis() + "-" + multipartFile.getOriginalFilename();
+        File savePath = new File(contextPath, imageName);
+        Thumbnails.of(multipartFile.getInputStream())
+                .scale(0.25)
+                .toFile(savePath);
+        photo.setPhotoThumUrl("http://localhost:8080/thumphoto/" + imageName);
     }
 }
--- src/test/java/com/ydb/WorkApplicationTests.java
+++ src/test/java/com/ydb/WorkApplicationTests.java
@@ -1,27 +1,24 @@
 package com.ydb;
 
+import net.coobird.thumbnailator.Thumbnails;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.Base64;
 
 //@RunWith(SpringRunner.class)
 //@SpringBootTest
 public class WorkApplicationTests {
 
-	@Test
-	public void contextLoads() {
-		String base64String="图片";
-		byte[] result = Base64.getEncoder().encode(base64String.getBytes());
-		System.out.println(new String(result));
-	}
-	public static byte[] decode(String source) {
-		try {
-			sun.misc.BASE64Decoder decoder = new sun.misc.BASE64Decoder();
-			return decoder.decodeBuffer(source);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void thum() throws IOException {
+        File file = new File("E:\\相册\\Camera Roll\\333055-106.jpg");
+        Thumbnails.of(file)
+                .scale(0.25)
+                .toFile("C:\\Users\\Administrator\\Desktop\\a380_200x300.jpg");
+    }
 }
